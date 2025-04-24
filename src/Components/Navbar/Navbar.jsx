e137cf5a4bc1b406afff1331c6acde414282e7db
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2025-04-24 14:42:16 +0700

    commit 3

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 
-const navbar = () => {
+const Navbar = () => {
+
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+        window.addEventListener('scroll', () => {
+          window.scrollY > 500 ? setSticky(true) : setSticky(false) 
+        })
+  }, [])
+
+
   return (
-    <nav className='container dark-nav'>
+    <nav className={`container ${sticky? 'dark-nav' :  ''}`}>
         <img src= {logo} alt="" className='logo' />
         <ul>
             <li>Home</li>
@@ -18,4 +28,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
